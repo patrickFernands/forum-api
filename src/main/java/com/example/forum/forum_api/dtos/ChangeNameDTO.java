b73a1b3fb3cf64d86397ca9ed055db11924335c2
Patrick <patrick.fernandes@estudante.ufla.br>
2026-07-23 18:24:20 -0300

    feat: implementa UserResource

---
+++ src/main/java/com/example/forum/forum_api/dtos/ChangeNameDTO.java
@@ -0,0 +1,5 @@
+package com.example.forum.forum_api.dtos;
+
+public record ChangeNameDTO(String name) {
+
+}
--- src/main/java/com/example/forum/forum_api/resources/UserResource.java
+++ src/main/java/com/example/forum/forum_api/resources/UserResource.java
@@ -3,15 +3,22 @@ package com.example.forum.forum_api.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.forum.forum_api.dtos.ChangeEmailDTO;
+import com.example.forum.forum_api.dtos.ChangeNameDTO;
+import com.example.forum.forum_api.dtos.ChangePasswordDTO;
+import com.example.forum.forum_api.dtos.UserIdDTO;
 import com.example.forum.forum_api.dtos.UserRegisterDTO;
 import com.example.forum.forum_api.dtos.UserRegisterResponseDTO;
 import com.example.forum.forum_api.entities.User;
 import com.example.forum.forum_api.services.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/users")
@@ -34,13 +41,49 @@ public class UserResource {
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 
-	/*
-	 * PUT /users/me/name ➔ Edita o próprio nome (changeName).
-	 * PUT /users/me/email ➔Edita o próprio e-mail (changeEmail). PUT
-	 * /users/me/password ➔ Edita a própria senha (changePassword).
-	 * DELETE /users/me ➔ Deleta a própria conta(deleteAccount).
-	 * PUT /users/{id}/ban ➔ Bane um usuário (Admin - banAccount).
-	 * PUT /users/{id}/unban ➔ Desbane um usuário (Admin - unbanAccount).
-	 */
+	@PutMapping("/{id}/name")
+	public ResponseEntity<Void> changeName(@PathVariable Long id, @RequestBody ChangeNameDTO entity) {
+
+		userService.changeName(id, entity.name());
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping("/{id}/email")
+	public ResponseEntity<Void> changeEmail(@PathVariable Long id, @RequestBody ChangeEmailDTO entity) {
+
+		userService.changeEmail(id, entity.email());
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping("/{id}/password")
+	public ResponseEntity<Void> changePassword(@PathVariable Long id, @RequestBody ChangePasswordDTO entity) {
+
+		userService.changePassword(id, entity.password());
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteAccount(@PathVariable Long id) {
+
+		userService.deleteAccount(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping("/{id}/ban")
+	public ResponseEntity<Void> ban(@PathVariable Long id, @RequestBody UserIdDTO entity) {
+
+		userService.banAccount(id, entity.id());
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping("/{id}/unban")
+	public ResponseEntity<Void> unban(@PathVariable Long id, @RequestBody UserIdDTO entity) {
+
+		userService.unbanAccount(id, entity.id());
+		return ResponseEntity.noContent().build();
+	}
 
 }
